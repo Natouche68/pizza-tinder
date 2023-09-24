c096c2bbdd3670cba1e2c6a9be3dca6c1dd5d0a9
Natouche68 <ngrossheny@gmail.com>
2023-09-24 10:42:59 +0200

    Create a basic UI

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,36 @@
+<script>
+	import ThumbsDown from "./lib/ThumbsDown.svelte";
+	import Heart from "./lib/Heart.svelte";
+</script>
+
 <h1>Pizza Tinder</h1>
+
+<div class="app">
+	<ThumbsDown />
+	<img src="pizzas/pepperoni pizza.png" alt="Pepperoni Pizza" class="pizza" />
+	<Heart />
+</div>
+
+<style>
+	h1 {
+		width: 90%;
+		margin: 3rem 5%;
+		border-bottom: solid 1px var(--discret-color);
+		text-align: center;
+		font-weight: 700;
+		font-size: 4rem;
+	}
+
+	.app {
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.pizza {
+		aspect-ratio: 1 / 1;
+		height: 80vh;
+		border-radius: 4rem;
+	}
+</style>
