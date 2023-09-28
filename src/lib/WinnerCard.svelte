95c8f96a2af31234b907d4751fcddd20b34f7f71
Natouche68 <ngrossheny@gmail.com>
2023-09-28 18:57:33 +0200

    Add an animation when revealing the winner

--- src/lib/WinnerCard.svelte
+++ src/lib/WinnerCard.svelte
@@ -25,17 +25,56 @@
 		font-weight: 500;
 		font-style: italic;
 		color: var(--discret-color);
+		animation: label-animation 1s ease 1s 1 normal both;
+	}
+
+	@keyframes label-animation {
+		from {
+			color: var(--text-color);
+			transform: scale(2) translateY(8rem);
+		}
+
+		to {
+			color: var(--discret-color);
+			transform: scale(1) translateY(0);
+		}
 	}
 
 	.name {
 		font-size: 7rem;
 		font-weight: 700;
 		margin: 2rem;
+		animation: name-animation 2s ease 2s 1 normal both;
+	}
+
+	@keyframes name-animation {
+		from {
+			opacity: 0;
+			filter: blur(2rem);
+		}
+
+		to {
+			opacity: 1;
+			filter: blur(0);
+		}
 	}
 
 	.image {
 		aspect-ratio: 1 / 1;
 		height: 60vh;
 		border-radius: 2rem;
+		animation: image-animation 2s ease 2s 1 normal both;
+	}
+
+	@keyframes image-animation {
+		from {
+			opacity: 0;
+			transform: scale(0);
+		}
+
+		to {
+			opacity: 1;
+			transform: scale(1);
+		}
 	}
 </style>
