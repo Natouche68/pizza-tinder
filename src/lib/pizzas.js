c1ba4d512f03aba7f7a20b62fbb66d1d91639f61
Natouche68 <ngrossheny@gmail.com>
2023-09-30 18:29:12 +0200

    Add more pizzas

--- src/lib/pizzas.js
+++ src/lib/pizzas.js
@@ -17,6 +17,34 @@ export const pizzaList = [
 	},
 	{
 		image: "pizzas/chocolate pizza.png",
-		name: "Chocolate Pizza",
+		name: "Chocolate Pizza with Bananas",
+	},
+	{
+		image: "pizzas/sushi pizza.png",
+		name: "Sushi Pizza",
+	},
+	{
+		image: "pizzas/salmone pizza.png",
+		name: "Salmone Pizza",
+	},
+	{
+		image: "pizzas/4 cheese pizza.png",
+		name: "4-Cheese Pizza",
+	},
+	{
+		image: "pizzas/napolitan pizza.png",
+		name: "Napolitan Pizza",
+	},
+	{
+		image: "pizzas/tarte flambee.png",
+		name: "Tarte Flambée",
+	},
+	{
+		image: "pizzas/mushroom pizza.png",
+		name: "Mushroom Pizza",
+	},
+	{
+		image: "pizzas/spicy pizza.png",
+		name: "Spicy Pizza",
 	},
 ];
